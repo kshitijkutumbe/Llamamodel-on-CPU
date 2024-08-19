40140ade22c5d67f018ddd9a6a151abde96414a2
kshitijkutumbe <kshitijkutumbe@gmail.com>
2024-08-19 13:37:30 +0530

    added app file

--- src/helper.py
+++ src/helper.py
@@ -9,8 +9,8 @@ please don't share false information."""
 
 
 
-#CUSTOM_SYSTEM_PROMPT="You are an advanced assistant that provides translation from English to Hindi"
-CUSTOM_SYSTEM_PROMPT="You are an advanced assistant that provides summarization given any book name"
+CUSTOM_SYSTEM_PROMPT="You are an advanced assistant that provides translation from English to Hindi"
+#CUSTOM_SYSTEM_PROMPT="You are an advanced assistant that provides summarization given any book name"
 
 
 
